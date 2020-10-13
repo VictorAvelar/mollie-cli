5e04de999d09258ff6248fc5b1b4ef1b8f3f45be
Victor Hugo Avelar Ossorio <aovo128446@upemor.edu.mx>
2020-10-13 20:34:46 +0200

    Handle errors on exec.Command for brose

--- pkg/utils/browse.go
+++ pkg/utils/browse.go
@@ -3,19 +3,21 @@ package utils
 import (
 	"os/exec"
 	"runtime"
+
+	"github.com/sirupsen/logrus"
 )
 
 // Browse opens the desired target in your native default browser.
 func Browse(target string) {
 	switch runtime.GOOS {
 	case "linux":
-		exec.Command("xdg-open", target).Start()
+		logrus.Fatal(exec.Command("xdg-open", target).Start())
 	case "windows":
-		exec.Command("rundll32", "url.dll,FileProtocolHandler", target).Start()
+		logrus.Fatal(exec.Command("rundll32", "url.dll,FileProtocolHandler", target).Start())
 	case "darwin":
-		exec.Command("open", target).Start()
+		logrus.Fatal(exec.Command("open", target).Start())
 	default:
 		// for the BSDs
-		exec.Command("xdg-open", target).Start()
+		logrus.Fatal(exec.Command("xdg-open", target).Start())
 	}
 }
