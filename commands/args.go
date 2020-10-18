1ad2b9c33dc836408ab932fcc940f707d446572f
Victor Hugo Avelar Ossorio <aovo128446@upemor.edu.mx>
2020-10-18 17:50:47 +0200

    Methods commands implementation

---
+++ commands/args.go
@@ -0,0 +1,14 @@
+package commands
+
+// Available command arguments.
+const (
+	AmountCurrencyArg = "amount-currency"
+	AmountValueArg    = "amount-value"
+	CurrencyArg       = "currency"
+	IDArg             = "id"
+	LocaleArg         = "locale"
+	SequenceTypeArg   = "sequence-type"
+	ResourceArg       = "resource"
+	BillingCountryArg = "billing-country"
+	WalletsArg        = "wallets"
+)
