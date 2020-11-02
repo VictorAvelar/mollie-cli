58c0d2cdcd47296c90de367db0912b71fc5f383b
victoravelar <deltatuts@gmail.com>
2020-11-02 15:59:02 +0100

    Fix payments command

--- commands/payments.go
+++ commands/payments.go
@@ -22,90 +22,180 @@ var (
 
 // Payments builds the payments command tree.
 func Payments() *command.Command {
-	p := &command.Command{
-		Command: &cobra.Command{
-			Use:     "payments",
-			Short:   "All operations to handle payments",
-			Aliases: []string{"pay", "p"},
+	p := command.Builder(
+		nil,
+		command.Config{
+			Namespace: "payments",
+			ShortDesc: "All operations to handle payments",
+			Aliases:   []string{"pay", "p"},
 		},
-	}
+		noCols,
+	)
 
 	lp := command.Builder(
 		p,
-		"list",
-		"Retrieve all payments created",
-		`Retrieve all payments created with the current website profile, 
+		command.Config{
+			Namespace: "list",
+			ShortDesc: "Retrieve all payments created",
+			LongDesc: `Retrieve all payments created with the current website profile, 
 ordered from newest to oldest. The results are paginated.`,
-		RunListPayments,
+			Execute: RunListPayments,
+		},
 		paymentsCols,
 	)
 
-	command.AddStringFlag(lp, FromArg, "", "", "offset the result set to the payment with this ID.", false)
-	command.AddIntFlag(lp, LimitArg, "", "the number of payments to return", 250, false)
+	command.AddFlag(lp, command.FlagConfig{
+		Name:  FromArg,
+		Usage: "offset the result set to the payment with this ID.",
+	})
+	command.AddFlag(lp, command.FlagConfig{
+		Name:  LimitArg,
+		Usage: "the number of payments to return",
+	})
 
 	gp := command.Builder(
 		p,
-		"get",
-		"Retrieve a single payment object by its payment token.",
-		``,
-		RunGetPayment,
-		[]string{},
+		command.Config{
+			Namespace: "get",
+			ShortDesc: "Retrieve a single payment object by its payment token.",
+			Execute:   RunGetPayment,
+		},
+		noCols,
 	)
 
-	command.AddStringFlag(gp, IDArg, "", "", "the payment token/id", true)
+	command.AddFlag(gp, command.FlagConfig{
+		Name:     IDArg,
+		Usage:    "the payment token/id",
+		Required: true,
+	})
 
 	cp := command.Builder(
 		p,
-		"cancel",
-		"Cancel a payment by its payment token.",
-		``,
-		RunCancelPayment,
-		[]string{},
+		command.Config{
+			Namespace: "cancel",
+			ShortDesc: "Cancel a payment by its payment token.",
+			Execute:   RunCancelPayment,
+		},
+		noCols,
 	)
 
-	command.AddStringFlag(cp, IDArg, "", "", "the payment token/id", true)
+	command.AddFlag(cp, command.FlagConfig{
+		Name:     IDArg,
+		Usage:    "the payment token/id",
+		Required: true,
+	})
 
 	cpp := command.Builder(
 		p,
-		"create",
-		"Payment creation",
-		``,
-		RunCreatePayment,
-		[]string{},
+		command.Config{
+			Namespace: "create",
+			ShortDesc: "Create a new payment",
+			Execute:   RunCreatePayment,
+			Aliases:   []string{"new", "start"},
+		},
+		noCols,
 	)
 
-	// Add common aliases
-	cpp.Aliases = []string{"new", "start"}
-	command.AddStringFlag(cpp, AmountValueArg, "", "", "a string containing the exact amount you want to charge in the given currency", true)
-	command.AddStringFlag(cpp, AmountCurrencyArg, "", "", "an ISO 4217 currency code", true)
-	command.AddStringFlag(cpp, DescriptionArg, "", "", "the description of the payment you’re creating to be show to your customers when possible", true)
-	command.AddStringFlag(cpp, RedirectURLArg, "", "", "the URL your customer will be redirected to after the payment process", true)
-	command.AddStringFlag(cpp, WebhookURLArg, "", "", "set the webhook URL, where we will send payment status updates to", false)
-	command.AddStringFlag(cpp, MetadataArg, "", "", "any data you like, for example a string or a JSON object", false)
-	command.AddStringFlag(cpp, MethodArg, "", "", "change the payment to a different payment method.", false)
-	command.AddStringFlag(cpp, LocaleArg, "", "", "update the language to be used in the hosted payment pages", false)
-	command.AddStringFlag(cpp, RPMToCountryArg, "", "", "parameter to restrict the payment methods available to your customer to those from a single country", false)
-	command.AddStringFlag(cpp, SequenceTypeArg, "", string(mollie.OneOffSequence), "indicate which type of payment this is in a recurring sequence", false)
-	command.AddStringFlag(cpp, CustomerIDArg, "", "", "the ID of the Customer for whom the payment is being created", false)
-	command.AddStringFlag(cpp, MandateIDArg, "", "", "when creating recurring payments, the ID of a specific Mandate may be supplied", false)
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:     AmountValueArg,
+		Usage:    "a string containing the exact amount you want to charge in the given currency",
+		Required: true,
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:     AmountCurrencyArg,
+		Usage:    "an ISO 4217 currency code",
+		Required: true,
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:     DescriptionArg,
+		Usage:    "the description of the payment you’re creating to be show to your customers when possible",
+		Required: true,
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:     RedirectURLArg,
+		Usage:    "the URL your customer will be redirected to after the payment process",
+		Required: true,
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:  WebhookURLArg,
+		Usage: "set the webhook URL, where we will send payment status updates to",
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:  MetadataArg,
+		Usage: "any data you like, for example a string or a JSON object",
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:  MethodArg,
+		Usage: "change the payment to a different payment method.",
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:  LocaleArg,
+		Usage: "update the language to be used in the hosted payment pages",
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:  RPMToCountryArg,
+		Usage: "parameter to restrict the payment methods available to your customer to those from a single country",
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:    SequenceTypeArg,
+		Usage:   "indicate which type of payment this is in a recurring sequence",
+		Default: string(mollie.OneOffSequence),
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:  CustomerIDArg,
+		Usage: "the ID of the Customer for whom the payment is being created",
+	})
+	command.AddFlag(cpp, command.FlagConfig{
+		Name:  MandateIDArg,
+		Usage: "when creating recurring payments, the ID of a specific Mandate may be supplied",
+	})
 
 	up := command.Builder(
 		p,
-		"update",
-		"Update some details of a created payment",
-		`There are also payment method specific parameters available, check the docs, please.`,
-		RunUpdatePayment,
-		[]string{},
+		command.Config{
+			Namespace: "update",
+			ShortDesc: "Update some details of a created payment",
+			LongDesc:  `There are also payment method specific parameters available, check the docs, please.`,
+			Execute:   RunUpdatePayment,
+		},
+		noCols,
 	)
 
-	command.AddStringFlag(up, IDArg, "", "", "the payment token/id", true)
-	command.AddStringFlag(up, DescriptionArg, "", "", "the description of the payment.", false)
-	command.AddStringFlag(up, RedirectURLArg, "", "", "the URL your customer will be redirected to after the payment process.", false)
-	command.AddStringFlag(up, WebhookURLArg, "", "", "set the webhook URL, where we will send payment status updates to.", false)
-	command.AddStringFlag(up, MetadataArg, "", "", "any data you like, for example a string or a JSON object", false)
-	command.AddStringFlag(up, MethodArg, "", "", "change the payment to a different payment method.", false)
-	command.AddStringFlag(up, LocaleArg, "", "", "update the language to be used in the hosted payment pages", false)
-	command.AddStringFlag(up, RPMToCountryArg, "", "", "parameter to restrict the payment methods available to your customer to those from a single country", false)
+	command.AddFlag(up, command.FlagConfig{
+		Name:     IDArg,
+		Usage:    "the payment token/id",
+		Required: true,
+	})
+	command.AddFlag(up, command.FlagConfig{
+		Name:     DescriptionArg,
+		Usage:    "the description of the payment to show to your customers when possible",
+		Required: true,
+	})
+	command.AddFlag(up, command.FlagConfig{
+		Name:     RedirectURLArg,
+		Usage:    "the URL your customer will be redirected to after the payment process",
+		Required: true,
+	})
+	command.AddFlag(up, command.FlagConfig{
+		Name:  WebhookURLArg,
+		Usage: "set the webhook URL, where we will send payment status updates to",
+	})
+	command.AddFlag(up, command.FlagConfig{
+		Name:  MetadataArg,
+		Usage: "any data you like, for example a string or a JSON object",
+	})
+	command.AddFlag(up, command.FlagConfig{
+		Name:  MethodArg,
+		Usage: "change the payment to a different payment method.",
+	})
+	command.AddFlag(up, command.FlagConfig{
+		Name:  LocaleArg,
+		Usage: "update the language to be used in the hosted payment pages",
+	})
+	command.AddFlag(up, command.FlagConfig{
+		Name:  RPMToCountryArg,
+		Usage: "parameter to restrict the payment methods available to your customer to those from a single country",
+	})
 
 	return p
 }
@@ -116,6 +206,12 @@ func RunListPayments(cmd *cobra.Command, args []string) {
 	ps, err := API.Payments.List(nil)
 	if err != nil {
 		logger.Fatal(err)
+	}
+
+	if verbose {
+		logger.Info("retrieved %d payments", ps.Count)
+		logger.Info("request target: %s", ps.Links.Self)
+		logger.Info("request docs: %s", ps.Links.Docs)
 	}
 
 	disp := displayers.MollieListPayments{
@@ -132,13 +228,18 @@ func RunListPayments(cmd *cobra.Command, args []string) {
 func RunGetPayment(cmd *cobra.Command, args []string) {
 	id := ParseStringFromFlags(cmd, IDArg)
 
-	if Verbose {
+	if verbose {
 		logger.Infof("retrieving payment with id (token) %s", id)
 	}
 
 	p, err := API.Payments.Get(id, nil)
 	if err != nil {
 		logger.Fatal(err)
+	}
+
+	if verbose {
+		logger.Info("request target: %s", p.Links.Self)
+		logger.Info("request docs: %s", p.Links.Documentation)
 	}
 
 	disp := displayers.MolliePayment{Payment: &p}
@@ -153,7 +254,7 @@ func RunGetPayment(cmd *cobra.Command, args []string) {
 func RunCancelPayment(cmd *cobra.Command, args []string) {
 	id := ParseStringFromFlags(cmd, IDArg)
 
-	if Verbose {
+	if verbose {
 		logger.Infof("canceling payment with id (token) %s", id)
 	}
 
@@ -162,7 +263,9 @@ func RunCancelPayment(cmd *cobra.Command, args []string) {
 		logger.Fatal(err)
 	}
 
-	if Verbose {
+	if verbose {
+		logger.Info("request target: %s", p.Links.Self)
+		logger.Info("request docs: %s", p.Links.Documentation)
 		logger.Info("payment successfully cancelled")
 		logger.Infof("cancellation processed at %s", p.CanceledAt)
 	}
@@ -190,7 +293,7 @@ func RunCreatePayment(cmd *cobra.Command, args []string) {
 	mandate := ParseStringFromFlags(cmd, MandateIDArg)
 	customer := ParseStringFromFlags(cmd, CustomerIDArg)
 
-	if Verbose {
+	if verbose {
 		logger.Infof("creating payment of %s %s", amount, currency)
 		PrintNonemptyFlagValue(RedirectURLArg, rURL)
 		PrintNonemptyFlagValue(DescriptionArg, desc)
@@ -231,7 +334,9 @@ func RunCreatePayment(cmd *cobra.Command, args []string) {
 		logger.Fatal(err)
 	}
 
-	if Verbose {
+	if verbose {
+		logger.Info("request target: %s", p.Links.Self)
+		logger.Info("request docs: %s", p.Links.Documentation)
 		logger.Info("payment successfully created")
 		logger.Infof("Payment created at %s", p.CreatedAt)
 	}
@@ -255,7 +360,7 @@ func RunUpdatePayment(cmd *cobra.Command, args []string) {
 	locale := ParseStringFromFlags(cmd, LocaleArg)
 	rpmCountry := ParseStringFromFlags(cmd, RPMToCountryArg)
 
-	if Verbose {
+	if verbose {
 		PrintNonemptyFlagValue(IDArg, id)
 		PrintNonemptyFlagValue(RedirectURLArg, rURL)
 		PrintNonemptyFlagValue(DescriptionArg, desc)
@@ -281,6 +386,12 @@ func RunUpdatePayment(cmd *cobra.Command, args []string) {
 	})
 	if err != nil {
 		logger.Fatal(err)
+	}
+
+	if verbose {
+		logger.Info("request target: %s", p.Links.Self)
+		logger.Info("request docs: %s", p.Links.Documentation)
+		logger.Info("payment successfully updated")
 	}
 
 	disp := displayers.MolliePayment{Payment: &p}
