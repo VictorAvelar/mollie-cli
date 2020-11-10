aed7731c4c64911a6d49a5b89e0d59ea751d722b
victoravelar <deltatuts@gmail.com>
2020-11-10 23:14:40 +0100

    Remove flags labeled as required on update payments

--- commands/payments.go
+++ commands/payments.go
@@ -187,14 +187,12 @@ ordered from newest to oldest. The results are paginated.`,
 		Required: true,
 	})
 	command.AddFlag(up, command.FlagConfig{
-		Name:     DescriptionArg,
-		Usage:    "the description of the payment to show to your customers when possible",
-		Required: true,
+		Name:  DescriptionArg,
+		Usage: "the description of the payment to show to your customers when possible",
 	})
 	command.AddFlag(up, command.FlagConfig{
-		Name:     RedirectURLArg,
-		Usage:    "the URL your customer will be redirected to after the payment process",
-		Required: true,
+		Name:  RedirectURLArg,
+		Usage: "the URL your customer will be redirected to after the payment process",
 	})
 	command.AddFlag(up, command.FlagConfig{
 		Name:  WebhookURLArg,
