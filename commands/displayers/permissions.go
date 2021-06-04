9280ab1aa91f1beec9e6babfc4dfc36058861074
Victor H. Avelar <aovo128446@upemor.edu.mx>
2021-06-04 10:34:51 +0200

    Update permission displayers

--- commands/displayers/permissions.go
+++ commands/displayers/permissions.go
@@ -20,6 +20,38 @@ func (mp *MolliePermissionList) KV() []map[string]interface{} {
 	return out
 }
 
+// Cols returns an array of columns available for displaying.
+func (mp *MolliePermissionList) Cols() []string {
+	return []string{
+		"RESOURCE",
+		"ID",
+		"DESCRIPTION",
+		"GRANTED",
+	}
+}
+
+// ColMap returns a list of columns and its description.
+func (mp *MolliePermissionList) ColMap() map[string]string {
+	return map[string]string{
+		"RESOURCE":    "the resource name specified by mollie",
+		"ID":          "the permissions id",
+		"DESCRIPTION": "the permission description",
+		"GRANTED":     "the permission status for the current api/access token",
+	}
+}
+
+// NoHeaders returns a boolean indicating if headers should be displayed
+// or not to the provided output.
+func (mp *MolliePermissionList) NoHeaders() bool {
+	return false
+}
+
+// Filterable indicates if the displayable output can be filtered
+// using the fields flag.
+func (mp *MolliePermissionList) Filterable() bool {
+	return true
+}
+
 // MolliePermission is wrapper for displaying.
 type MolliePermission struct {
 	*mollie.Permission
@@ -31,6 +63,38 @@ func (p *MolliePermission) KV() []map[string]interface{} {
 	x := buildXPermission(p.Permission)
 	out = append(out, x)
 	return out
+}
+
+// Cols returns an array of columns available for displaying.
+func (p *MolliePermission) Cols() []string {
+	return []string{
+		"RESOURCE",
+		"ID",
+		"DESCRIPTION",
+		"GRANTED",
+	}
+}
+
+// ColMap returns a list of columns and its description.
+func (p *MolliePermission) ColMap() map[string]string {
+	return map[string]string{
+		"RESOURCE":    "the resource name specified by mollie",
+		"ID":          "the permissions id",
+		"DESCRIPTION": "the permission description",
+		"GRANTED":     "the permission status for the current api/access token",
+	}
+}
+
+// NoHeaders returns a boolean indicating if headers should be displayed
+// or not to the provided output.
+func (p *MolliePermission) NoHeaders() bool {
+	return false
+}
+
+// Filterable indicates if the displayable output can be filtered
+// using the fields flag.
+func (p *MolliePermission) Filterable() bool {
+	return true
 }
 
 func buildXPermission(mp *mollie.Permission) map[string]interface{} {
