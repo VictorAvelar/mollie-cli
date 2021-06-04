0f43bc19ffb85d463445b6ceaaaca9bbc2c1619f
Victor H. Avelar <aovo128446@upemor.edu.mx>
2021-06-04 09:23:42 +0200

    Update tooling commands to admiral

--- commands/tooling.go
+++ commands/tooling.go
@@ -1,35 +1,15 @@
 package commands
 
 import (
-	"fmt"
-
-	"github.com/VictorAvelar/mollie-cli/internal/command"
+	"github.com/avocatl/admiral/pkg/commander"
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
 
-// Version creates the version command.
-func Version() *command.Command {
-	return command.Builder(
+func docs() *commander.Command {
+	return commander.Builder(
 		nil,
-		command.Config{
-			Namespace: "version",
-			ShortDesc: "Displays the current command version",
-			Execute: func(cmd *cobra.Command, args []string) {
-				fmt.Printf("Mollie CLI %s\n", version)
-			},
-			Aliases: []string{"v", "ver"},
-			Example: "mollie version",
-		},
-		noCols,
-	)
-}
-
-// Docs creates the version command.
-func Docs() *command.Command {
-	return command.Builder(
-		nil,
-		command.Config{
+		commander.Config{
 			Namespace: "docs",
 			ShortDesc: "Generates markdown documentation",
 			Execute: func(cmd *cobra.Command, args []string) {
@@ -41,6 +21,6 @@ func Docs() *command.Command {
 			Hidden:  true,
 			Example: "mollie docs",
 		},
-		noCols,
+		commander.NoCols(),
 	)
 }
